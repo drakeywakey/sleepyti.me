1232d12c0e689892537b04dbf689a27c668ae172
Drake Bennion <drakebennion@gmail.com>
2016-12-22 10:21:14 -0700

    add bodyParser for form posting

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.use("/styles", express.static(__dirname + '/styles'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, function () {
 	console.log('listening on port 3000');
@@ -8,4 +10,8 @@ app.listen(3000, function () {
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/sleepEntry', function (req, res) {
+	console.log(req.body);
 });
