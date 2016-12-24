a649dcee3472c06c734c4332c2149d2eece9ebf9
Drake Bennion <drakebennion@gmail.com>
2016-12-24 08:38:34 -0700

    entries are being saved in mongo

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var Calculator = require('./js/SleepCalculator.js');
 var MongoClient = require('mongodb').MongoClient;
+var db = null;
 var dbuser = 'drakeywakey';
 var dbpassword = 'drakobian';
 var mongoUrl = 'mongodb://' + dbuser + ':' + dbpassword + '@ds145188.mlab.com:45188/sleep-times';
@@ -12,8 +13,16 @@ app.use('/js', express.static(__dirname + '/js'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-app.listen(3000, function () {
-	console.log('listening on port 3000');
+MongoClient.connect(mongoUrl, function (err, datab) {
+	if (err) {
+		return console.error(err);
+	}
+	else {
+		db = datab;
+		app.listen(3000, function () {
+			console.log('listening on port 3000');
+		});
+	}
 });
 
 app.get('/', function (req, res) {
@@ -21,19 +30,20 @@ app.get('/', function (req, res) {
 });
 
 app.post('/sleepEntry', function (req, res) {
-	var data = {};
+	var data = req.body;
+	data.date = new Date().toDateString();
 
 	calculator = new Calculator();
-	calculator.calculateSleep(req.body);
+	calculator.calculateSleep(data);
+
+	db.collection('test').save(data, function (err, result) {
+		if (err) {
+			return console.error(err);
+		}
+		else {
+			console.log('saved to the db');
+		}
+	});
 
 	res.render('./index.ejs', calculator);
-});
-
-MongoClient.connect(mongoUrl, function (err, db) {
-	if (err) {
-		console.error(err);
-	}
-	else {
-		console.log(db);
-	}
 });
